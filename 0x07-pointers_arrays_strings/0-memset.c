917c8d06bfcecc879a796f0fe166096fe798ecd2
ngqazz <ngqazz@gmail.com>
2022-09-26 16:27:45 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,38 +1,19 @@
-#include "main.h"                                                                                                                                               /**                                                                                                                                  
- * _memset - Fills the first n bytes of the memory area                          #include "main.h"
- *
- */**
-* *_memset - fills memory with a constant byte.
-* * @s: pointer to put the constant
-* * @b: constant
-* * @n: max bytes to use
-* * Return: s
-* * _memset - fills the first @n bytes of the memory area pointed
-* * to by @s with the constant byte of @b
-* *
-* * @s: Pointer to a memory area
-* *
-* * @b: Value that is to fill up memory area @s
-* *
-* * @n: number of bytes of memory area @s to be filled with @b
-* *
-* * Return: Pointer to the memory area @s
-* */
+#include "main.h"
 
+/**
+ *_memset - fills a memory block with a constant byte
+ *@s: address to memory block
+ *@b: char to be used
+ *@n: number of bytes to be used
+ *
+ *Return: pointer to the memory block
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-		unsigned int i;
-
-		for (i = 0; n > 0 ; i++, n--)
-		{
-				s[i] = b;
-		}
-			for (i = 0; i < n; i++)
-					{
-								*(s + i) = b;
-									}
-
-			return (s);
-				return (s);
-}                                                                                           
+        while (n)
+        {
+                s[n - 1] = b;
+                n--;
+        }
+        return (s);
+}
