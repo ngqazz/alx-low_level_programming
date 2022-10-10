f55bfd87ca28db7522df49fa2056c1d4e12e511a
ngqazz <ngqazz@gmail.com>
2022-10-10 16:57:55 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
-#endif
-
+#ifndef ABS_H
+#define ABS_H
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif /* ABS_H */
