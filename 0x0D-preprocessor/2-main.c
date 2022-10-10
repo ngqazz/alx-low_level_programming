b6c6bd10e86d34fbff58a32f43073f0917cfe576
ngqazz <ngqazz@gmail.com>
2022-10-10 16:54:53 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
 /**
-* main - Entry point, prints file name it was compiled from
-*
-* Return: 0 (Success)
-*/
+ *main- prints file name
+ *
+ *Return: Always 0
+ */
 
 int main(void)
 {
+
 	printf("%s\n", __FILE__);
+
 	return (0);
 }
