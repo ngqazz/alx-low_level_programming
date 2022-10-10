632a9f6359fdcebc42e0aff6fb56626e22400e4c
ngqazz <ngqazz@gmail.com>
2022-10-10 06:53:09 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include "main.h"
+#include "dog.h"
+/**
+ * init_dog - intialization of the dog structure
+ * @d: a dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner ogf the dog
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog = {.name = name, .age = age, .owner = owner};
+}
